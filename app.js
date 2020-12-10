2dac7e3a4d20a3e450c365203272399863b8000e
Leslie Ledeboer <leslie.ledeboer@gmail.com>
2020-12-10 15:26:13 -0800

    Attempt to get access token

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-var authCode = window.location.search.match('code=(.*)')[1];
+// var state;
 
-let authData = {
-	grant_type: 'authorization_code',
-	code: authCode,
-	redirect_uri: 'https://leslieledeboer.github.io/SearchSaved'
-}
+// if (window.location.search == null) {
+// 	state = Math.floor(Math.random()*16777215).toString(16);
+
+// 	var url1 = "https://www.reddit.com/api/v1/authorize?client_id=dYnhP5SHz6XIeA&response_type=code&state=";
+// 	var url2 = "&redirect_uri=https%3A%2F%2Fleslieledeboer.github.io%2FSearchSaved%2F&duration=permanent&scope=identity";
+
+// 	var url = url1.concat(state.concat(url2));
+
+// 	document.getElementById("direct").href = url;
+// }
+
+// else if (window.location.search.match('state=(.*')[0] == state) {
+
+// }
+
+var authCode = window.location.search.match('code=(.*)')[1];
 
 fetch ('https://www.reddit.com/api/v1/access_token', {
 	method: 'POST',
 	headers: {
-		"Content-Type": "application/json"
+		"Content-Type": "application//x-www-form-urlencoded"
 	},
-	body: JSON.stringify(authData)
+	body: grant_type='authorization_code'&code=authCode&redirect_uri='https://leslieledeboer.github.io/SearchSaved'
 })
 .then(response => response.json())
 .then(data => {

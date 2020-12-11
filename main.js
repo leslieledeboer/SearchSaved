98c25d146966146af8650430fe5484469dffdd88
Leslie Ledeboer <leslie.ledeboer@gmail.com>
2020-12-11 14:28:32 -0800

    Display posts

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ async function main() {
   const container = document.getElementById("post_container");
 
   for (let i = 0; i < posts.length; i++) {
-  	markup += `<a class="post href="https://www.reddit.com/${posts[i].permalink}"></a>
+  	markup += `<a class="post" href="https://www.reddit.com/${posts[i].permalink}"></a>
   	  <div class="title">${posts[i].title}</div>
   	  <div class="author">${posts[i].author.name}</div><br><br>`;
   }

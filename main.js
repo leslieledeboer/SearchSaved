375f96e803eeba4a9f7a862cfe779c6dca33a6c2
Leslie Ledeboer <leslie.ledeboer@gmail.com>
2020-12-11 12:00:27 -0800

    Try to refresh

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
 var code = new URL(window.location.href).searchParams.get('code');
+var state = new URL(window.location.href).serachParams.get('state');
 
-if (code !== null) {
+localStorage.clear();
+
+if (code !== null && state === 'jeff') {
   snoowrap.fromAuthCode({
   code: code,
   clientId: 'anDof_QS7pjDyw',
   redirectUri: 'https://leslieledeboer.github.io/SearchSaved/main.html'
 }).then(r => {
-  console.log(r.refreshToken);
-  console.log(r.accessToken);
-  localStorage.setItem("refresh", r.refreshToken);
-  localStorage.setItem("access", r.accessToken);
+  sessionStorage.setItem("refresh", r.refreshToken);
+  sessionStorage.setItem("access", r.accessToken);
   r.getMe().getSavedContent().then(listing => {
-  	console.log(listing[0].title);
-  	console.log(listing[0].permalink);
-  	console.log(listing[0].author.name);
-  	console.log(listing[0]);
-
   	document.getElementById("title").innerHTML = listing[0].title;
   	document.getElementById("link").innerHTML = listing[0].permalink;
   	document.getElementById("author").innerHTML = listing[0].author.name;
+  	state = 'notjeff';
   });
 }).catch((error) => {
   console.error('Error:', error);
 });
+}
+
+else {
+	var snoo = new snoowrap({
+		clientId: 'anDof_QS7pjDyw',
+		refreshToken: sessionStorage.getItem("refresh"),
+		accessToken: sessionStorage.getItem("access")
+	}).then(r => {
+	  r.getMe().getSavedContent().then(listing => {
+  	    document.getElementById("title").innerHTML = listing[0].title;
+  	    document.getElementById("link").innerHTML = listing[0].permalink;
+  	    document.getElementById("author").innerHTML = listing[0].author.name;
+	});
 }

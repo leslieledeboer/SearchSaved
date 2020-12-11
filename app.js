dc2d39b7052d65c02dc7492a72fc00709d2a1522
Leslie Ledeboer <leslie.ledeboer@gmail.com>
2020-12-11 08:34:00 -0800

    debug

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+window.addEventListener('unhandledrejection', function(event) {
+  // the event object has two special properties:
+  alert(event.promise); // [object Promise] - the promise that generated the error
+  alert(event.reason); // Error: Whoops! - the unhandled error object
+});
+
 var authenticationUrl = snoowrap.getAuthUrl({
   clientId: 'anDof_QS7pjDyw',
   scope: ['identity'],

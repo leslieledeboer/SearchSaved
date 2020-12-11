a2086d83d2ee96af5fdcf0910f16b6682eab70f8
Leslie Ledeboer <leslie.ledeboer@gmail.com>
2020-12-11 12:31:46 -0800

    Add username

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var code = new URL(window.location.href).searchParams.get('code');
-// var state = new URL(window.location.href).searchParams.get('state');
 
 if (!sessionStorage.getItem("refresh")) {
   snoowrap.fromAuthCode({
@@ -9,6 +8,11 @@ if (!sessionStorage.getItem("refresh")) {
 }).then(r => {
   sessionStorage.setItem("refresh", r.refreshToken);
   sessionStorage.setItem("access", r.accessToken);
+
+  r.getMe().then(user => {
+  	document.getElementById("username").innerHTML = user.name;
+  })
+
   r.getMe().getSavedContent().then(listing => {
   	document.getElementById("title").innerHTML = listing[0].title;
   	document.getElementById("link").innerHTML = listing[0].permalink;
